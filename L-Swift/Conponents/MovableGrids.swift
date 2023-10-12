08aad4f1fc63066a93c86dfb1a4f972babf6edec
Xuân Quý <81242756+xqsadness@users.noreply.github.com>
2023-10-12 14:10:45 +0700

    MovableGrids

---
+++ L-Swift/Conponents/MovableGrids.swift
@@ -0,0 +1,20 @@
+//
+//  MovableGrids.swift
+//  L-Swift
+//
+//  Created by darktech4 on 12/10/2023.
+//
+
+import SwiftUI
+
+struct MovableGrids: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MovableGrids_Previews: PreviewProvider {
+    static var previews: some View {
+        MovableGrids()
+    }
+}
