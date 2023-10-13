f282f3e400d13ed744460f3f5060dd88cc97ec33
Xuân Quý <81242756+xqsadness@users.noreply.github.com>
2023-10-13 16:12:04 +0700

    Strength Meter with Progress & Color Feedback

---
+++ L-Swift/Conponents/PasswordStrength.swift
@@ -0,0 +1,127 @@
+//
+//  PasswordStrength.swift
+//  L-Swift
+//
+//  Created by darktech4 on 13/10/2023.
+//
+
+import SwiftUI
+
+struct PasswordStrength: View {
+    @State var text = ""
+    @State var progress: CGFloat = 0
+    @State var checkMinChars = false
+    @State var checkLetter = false
+    @State var checkPunctuation = false
+    @State var checkNumber = false
+    @State var showPassword = false
+    
+    var progressColor: Color{
+        let containsLetters = text.rangeOfCharacter(from: .letters) != nil
+        
+        let containsNumbers = text.rangeOfCharacter(from: .decimalDigits) != nil
+        
+        let containsPunctuation = text.rangeOfCharacter(from: CharacterSet(charactersIn: "!@#%^&")) != nil
+        
+        if containsLetters && containsNumbers && containsPunctuation && text.count >= 8{
+            return Color.green
+        }else if containsLetters && !containsNumbers && !containsPunctuation{
+            return Color.red
+        }else if containsNumbers && !containsLetters && !containsPunctuation{
+            return Color.red
+        }else if containsLetters && containsNumbers && !containsPunctuation{
+            return Color.yellow
+        }else if containsLetters && containsNumbers && containsPunctuation{
+            return Color.blue
+        }else {
+            return Color.gray
+        }
+    }
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 40){
+            VStack(alignment: .leading, spacing: 10) {
+                CheckText (text: "Minimum 8 characters", check: $checkMinChars)
+                CheckText (text: "At least one letter", check: $checkLetter)
+                CheckText (text: "(I@#$%*^&)", check: $checkPunctuation)
+                CheckText (text: "Number", check: $checkNumber)
+            }
+            
+            ZStack{
+                if !showPassword{
+                    SecureField("Password", text: $text)
+                        .padding(.leading).bold()
+                        .onChange(of: text, perform: { newValue in
+                            withAnimation {
+                                progress = min(1.0, max(0, CGFloat(newValue.count) / 8.0))
+                                
+                                checkMinChars = newValue .count >= 8
+                                checkLetter = newValue.rangeOfCharacter(from: .letters) != nil
+                                checkNumber = newValue.rangeOfCharacter(from: .decimalDigits) != nil
+                                checkPunctuation = newValue.rangeOfCharacter(from:
+                                                                                CharacterSet (charactersIn: "!©#%^&")) != nil
+                            }
+                        })
+                        .frame(height: 60)
+                        .background(
+                            RoundedRectangle(cornerRadius: 10).foregroundColor (.white)
+                                .shadow(color: .black.opacity(0.2), radius: 5, x: 10, y: 4)
+                        )
+                }else{
+                    TextField("Password", text: $text)
+                        .padding(.leading).bold()
+                        .onChange(of: text, perform: { newValue in
+                            withAnimation {
+                                progress = min(1.0, max(0, CGFloat(newValue.count) / 8.0))
+                                
+                                checkMinChars = newValue .count >= 8
+                                checkLetter = newValue.rangeOfCharacter(from: .letters) != nil
+                                checkNumber = newValue.rangeOfCharacter(from: .decimalDigits) != nil
+                                checkPunctuation = newValue.rangeOfCharacter(from:
+                                                                                CharacterSet (charactersIn: "!©#%^&")) != nil
+                            }
+                        })
+                        .frame(height: 60)
+                        .background(
+                            RoundedRectangle(cornerRadius: 10).foregroundColor (.white)
+                                .shadow(color: .black.opacity(0.2), radius: 5, x: 10, y: 4)
+                        )
+                }
+                
+                RoundedRectangle (cornerRadius: 10) .trim(from: 0, to: progress)
+                    .stroke(progressColor,lineWidth: 3)
+                    .frame(height: 60)
+                    .rotationEffect(.degrees(-180))
+            }
+            .overlay(alignment: .trailing){
+                Image(systemName: showPassword ? "eye.fill" : "eye.slash.fill")
+                    .padding(.trailing, 10)
+                    .onTapGesture {
+                        withAnimation {
+                            showPassword.toggle()
+                        }
+                    }
+            }
+        }
+        .padding ()
+    }
+}
+
+
+struct PasswordStrength_Previews: PreviewProvider {
+    static var previews: some View {
+        PasswordStrength()
+    }
+}
+
+struct CheckText: View {
+    let text: String
+    @Binding var check:Bool
+    var body: some View {
+        HStack{
+            Image (systemName: check ? "checkmark.circle.fill" : "circle")
+            Text (text)
+        }
+        .foregroundColor (check ? .green : .gray)
+    }
+}
--- L-Swift/ContentView.swift
+++ L-Swift/ContentView.swift
@@ -8,9 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var show = true
+    
     var body: some View {
         VStack{
-            HomeCompositional()
+            /*
+             //Lazy Compositional Layout - Pagination - JSON Parsing Async/Await
+             HomeCompositional()
+             
+             //Particle Effects - Animations
+             ParticleEffectsHome()
+             
+             //Tabbar2
+             HomeTabbar2View()
+             
+             //HomeOnBoarding2
+             HomeOnBoarding2()
+             
+             //HomeOnboarding
+             OnboardingView()
+             
+             //Side menu
+             SideMenuView(show: $show)
+             
+             // Movable Grids in 5 Mins - Drag & Drop
+             MovableGrids()
+             
+             //Arc Menu Button
+             ArcMenuButton(buttons: Array(ArcMenuButtonName.allCases)) { button in
+             switch button {
+             case .capture:
+             print("Capture button tapped.")
+             case .star:
+             print("star button tapped.")
+             case .noti:
+             print("noti button tapped.")
+             case .bookmark:
+             print("bookmark button tapped.")
+             }
+             }
+             
+             //Strength Meter with Progress & Color Feedback
+             PasswordStrength()
+             
+             */
         }
     }
 }
