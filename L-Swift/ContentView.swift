2bae40ddf29cb5dc14c0b1cada3c1358a551af1c
Xuân Quý <81242756+xqsadness@users.noreply.github.com>
2024-06-11 17:43:58 +0700

    add custom segment, refactor

--- L-Swift/ContentView.swift
+++ L-Swift/ContentView.swift
@@ -8,54 +8,93 @@
 import SwiftUI
 
 struct ContentView: View {
-//    @State var show = true
-//    @State var searchText = ""
+    @State var show = true
+    @State var searchText = ""
     
     var body: some View {
-        VStack{
-            /*
-             //Lazy Compositional Layout - Pagination - JSON Parsing Async/Await
-             HomeCompositional()
-             
-             //Particle Effects - Animations
-             ParticleEffectsHome()
-             
-             //Tabbar2
-             HomeTabbar2View()
-             
-             //HomeOnBoarding2
-             HomeOnBoarding2()
-             
-             //HomeOnboarding
-             OnboardingView()
-             
-             //Side menu
-             SideMenuView(show: $show)
-             
-             // Movable Grids in 5 Mins - Drag & Drop
-             MovableGrids()
-             
-             //Arc Menu Button
-             ArcMenuButton(buttons: Array(ArcMenuButtonName.allCases)) { button in
-             switch button {
-             case .capture:
-             print("Capture button tapped.")
-             case .star:
-             print("star button tapped.")
-             case .noti:
-             print("noti button tapped.")
-             case .bookmark:
-             print("bookmark button tapped.")
-             }
-             }
-             
-             //Strength Meter with Progress & Color Feedback
-             PasswordStrength()
-             
-             // Search bar
-             CustomSearchbar(searchText: $searchText)
-             */
-            BubbleTransition()
+        NavigationStack{
+            ScrollView(showsIndicators: false){
+                VStack(spacing: 15) {
+                    navigationScreen("Compositional") {
+                        HomeCompositional()
+                    }
+                    
+                    navigationScreen("Particle Effects") {
+                        ParticleEffectsHome()
+                    }
+                    
+                    navigationScreen("Tab bar") {
+                        HomeTabbar2View()
+                    }
+                    
+                    navigationScreen("Side Menu") {
+                        SideMenuView(show: $show)
+                    }
+                    
+                    navigationScreen("Onboarding") {
+                        OnboardingView()
+                    }
+                    
+                    navigationScreen("Onboarding 2") {
+                        HomeOnBoarding2()
+                    }
+                    
+                    navigationScreen("Movable Grids") {
+                        MovableGrids()
+                    }
+                    
+                    navigationScreen("Arc Menu Button") {
+                        ArcMenuButton(buttons: Array(ArcMenuButtonName.allCases)) { button in
+                            switch button {
+                            case .capture:
+                                print("Capture button tapped.")
+                            case .star:
+                                print("star button tapped.")
+                            case .noti:
+                                print("noti button tapped.")
+                            case .bookmark:
+                                print("bookmark button tapped.")
+                            }
+                        }
+                    }
+                    
+                    navigationScreen("Password Strength") {
+                        PasswordStrength()
+                    }
+                    
+                    navigationScreen("Custom Search bar") {
+                        CustomSearchbar(searchText: $searchText)
+                    }
+                    
+                    navigationScreen("Bubble Transition") {
+                        BubbleTransition()
+                    }
+                    
+                    navigationScreen("Segment Animated") {
+                        SegmentAnimatedView()
+                    }
+                }
+                .padding()
+            }
+        }
+    }
+    
+    @ViewBuilder
+    func navigationScreen<Content: View>(_ title: String, @ViewBuilder content: @escaping () -> Content) -> some View {
+        NavigationLink {
+            content()
+        } label: {
+            HStack {
+                Text("\(title)")
+                    .font(.title3)
+                    .foregroundStyle(Color.text)
+                
+                Spacer()
+                
+                Image(systemName: "chevron.right")
+                    .imageScale(.large)
+                    .foregroundStyle(Color.text)
+            }
         }
     }
 }
